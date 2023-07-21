abb38937e8268087b542be62904d424e79ebe8a0
Levi Wolfe <wolfel@ymail.com>
2023-07-21 00:07:53 -0500

    Webpack config

--- strategy/webpack.config.js
+++ strategy/webpack.config.js
@@ -1,16 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-    rules: [
-        {
-            test: /\.tsx?$/,
-            use: "ts-loader",
-            exclude: /node_modules/,
-        },
-    ],
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                },
+            },
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+
     resolve: {
         extensions: [".js", ".jsx", ".ts", ".tsx"],
     },
+
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+            chunkFilename: "[id].css",
+        }),
+    ],
 };
